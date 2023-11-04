29257d5052f24a03e5cda0f8ff81f865a7f8ac3d
Bradley Miner <bjjrminer@comcast.net>
2023-11-04 17:45:47 -0600

    Refined comments valid_parentheses

--- leetcode-solutions/20_valid_parentheses.py
+++ leetcode-solutions/20_valid_parentheses.py
@@ -1,28 +1,32 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        
+        # This is the compliments dictionary
         brackets = {
             "(": ")",
             "[": "]",
             "{": "}",
         }
         
+        # Stack to track the opening and closing of parentheses. We will use LIFO.
         stack = []
 
+        # If uneven length, impossible to be valid.
         if (len(s) % 2) > 0:
             return False
         
         for i in s:
-            if i in brackets:
-                stack.append(i)
-            if i not in brackets:
-                if len(stack) == 0:
-                    return False
-                popped = stack.pop()
-                if brackets[popped] != i:
+            if i in brackets: # This will only result True if "i" matches a key, which are opening brackets.
+                stack.append(i) # add the OPENING parenthesis. 
+            if i not in brackets: # If its not in brackets, it could be invalid, or a closing bracket.
+                if len(stack) == 0: # If stack is currently empty, then means no opening bracket was previously encountered.
+                    return False # -> invalid parentheses
+                popped = stack.pop() # get the last added opening bracket.
+                if brackets[popped] != i: # Use the brackets dictionary to check if "i" is proper closing bracket.
                     return False
 
-        if len(stack) == 0:
-            return True
+        if len(stack) == 0: # length of our tracking stack should be zero at the end.
+            return True # -> VALID parentheses
         else:
             return False
         
