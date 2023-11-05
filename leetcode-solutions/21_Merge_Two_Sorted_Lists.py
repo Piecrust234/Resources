23324716bb3f3624a2f8e23550602cdbb0812ca9
Bradley Miner <bjjrminer@comcast.net>
2023-11-04 19:05:45 -0600

    Adjustments to comments merge two sorted lists

--- leetcode-solutions/21_Merge_Two_Sorted_Lists.py
+++ leetcode-solutions/21_Merge_Two_Sorted_Lists.py
@@ -13,8 +13,6 @@ def printList(resultList):
         printList(resultList.next)
 
 class Solution:
-
-    
     def mergeTwoLists(self, list1, list2):
 
        
@@ -25,13 +23,13 @@ class Solution:
         while list1 and list2: # When the current value of each list is not None/empty, iterate.
             if list1.val < list2.val: # if list1.val is less than list2.val, list1.val will be popped in first.
                 tail.next = list1 # setting what follows after the current tail to be this next lowest value
-                list1 = list1.next # get the next value of the list
+                list1 = list1.next # get the next value of the list1 (ie if list is 1,2,4, move to '2')
             else: # Reverse of the logic above
                 tail.next = list2
                 list2 = list2.next
             tail = tail.next # for the next iteration, set the tail to be the end again.
 
-        # we reach the following logic if either of the list1 or the list2 are empty; in this case, what remains (either list1 or list2), can be slapped on at the end
+        # we reach the following logic if either of the list1 or the list2 are empty (no more next); in this case, what remains (either list1 or list2), can be slapped on at the end
         if list1: 
             tail.next = list1
         elif list2:
